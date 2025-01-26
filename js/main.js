c0db4bae3a8b09a70c7aa2b93865aa8e0b5a945d
Connie Sham <155795436+ConnieSham823@users.noreply.github.com>
2025-01-26 00:38:56 -0500

    Update main.js for AJAX
    In this commit was updated the main.js for AJAX set up stage.

---
+++ js/main.js
@@ -0,0 +1,8 @@
+(() => {
+    const characterList = document.querySelector("#character-list");
+    const movietemplate = document.querySelector("#movie-template");
+    const movieCon = document.querySelector("#movie-con");
+    const baseURL = "https://swapi.dev/"
+
+
+})();
