b3721ef55668352cd8137b13849b71e3465059b3
Connie Sham <155795436+ConnieSham823@users.noreply.github.com>
2025-01-26 16:34:24 -0500

    update main.js for character and movie
    In this commit updated main.js for call data from API on character and movie.

--- js/main.js
+++ js/main.js
@@ -2,19 +2,19 @@
     const characterList = document.querySelector("#character-list");
     const movietemplate = document.querySelector("#movie-template");
     const movieCon = document.querySelector("#movie-con");
-    const baseURL = "https://swapi.dev/api";
+    const baseURL = "https://swapi.dev/";
 
     function getCharacters() {
-        fetch(`${baseURL}people`)
-        .then(response => response.jason())
+        fetch(`${baseURL}api/people`)
+        .then(response => response.json())
         .then(function(response){
-            const characters = response.description;
+            const characters = response.results;
             const ul = document.createElement("ul");
             characters.forEach(character => {
                 const li = document.createElement("li");
                 const a = document.createElement("a");
-                a.textContent = character["name"];
-                a.dataset.review = character["films"];
+                a.textContent = character.name;
+                a.dataset.review = character.films;
                 li.appendChild(a);
                 ul.appendChild(li);
             });
@@ -31,6 +31,35 @@
             console.log(err);
         })
     }
+
+    getCharacters();
+
+    function getMovie(e) {
+        const filmURLs = e.currentTarget.dataset.review;
+
+        fetch(`${baseURL}api/film${filmURLs}`)
+        .then(response => response.json())
+        .then(function(response){
+            movieCon.innerHTML = "";
+            console.log(response.results);
+            const clone = movietemplate.textContent.cloneNode(true);
+
+            const movieDescription = clone.querySelector(".movie-description");
+            movieDescription.innerHTML = response.result.opening_crawl;
+
+            const movieTitle = clone.querySelector(".movie-title");
+            movieTitle.innerHTML = response.result.title;
+
+            movieCon.appendChild(clone);
+
+        })
+        .catch(function(err){
+            movieCon.innerHTML = "<p>No review available for this section. </p>"
+        })
+
+    }
+
+    getMovies();
 
     
 })();
